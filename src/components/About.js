11691f27d0802ea97ad256fbebb2302ed8b50bc9
shu Lin <bbqlinshu@gmail.com>
2022-01-13 21:41:36 -0500

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+  const {bio, github, linkedin} = props
+  const isPassDown = (bio === undefined || bio.length === 0)
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {isPassDown ? null:<p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={github} linkedin={linkedin}/>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,14 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
+  const{color, name, city, bio, links} = user
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={color} username={name} city={city}/>
+      <About bio={bio} github={links.github} linkedin={links.linkedin}/>
     </div>
   );
 }
